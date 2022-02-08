fc832221a0cd0c3e1009f163e5dbd139b8b63d32
FabrizioAdi <adriannoszczyk@gmail.com>
2022-02-08 17:30:35 +0000

    Create post models in models.py file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    options = (
+        ('draft', 'Draft')
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    publish_date = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=options, default='draft')
